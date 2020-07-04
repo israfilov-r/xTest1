d0a4f2cbf587bcc0c4e389ad68641c0a2fa8bb5e
Rasim Israfilov <israfilov-r@yandex.ru>
2020-07-04 22:38:32 +0300

    123

---
+++ xTest1/xC_Cnsl_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace xC_Cnsl_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            var cfgCS = System.Configuration.ConfigurationManager.ConnectionStrings;
+
+
+        }
+    }
+}
